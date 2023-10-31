1739c1b2ae5dd3b8cd11515f7c1935aa28f0b732
Devin Lehmacher <lehmacdj@gmail.com>
2023-10-31 01:03:38 -0400

    Initial Commit

---
+++ MemoryRetentionTest/MemoryRetentionTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryRetentionTestApp.swift
+//  MemoryRetentionTest
+//
+//  Created by Devin Lehmacher on 10/31/23.
+//
+
+import SwiftUI
+
+@main
+struct MemoryRetentionTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
